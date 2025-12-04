40d7c18b28d0f02c2cad399b0086420bb0482211
manju811943 <manju811943@gmail.com>
2025-12-04 23:06:07 +0530

    initial committed

---
+++ src/main/java/com/trail_docker/TrailDockerApplication.java
@@ -0,0 +1,13 @@
+package com.trail_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trail_docker/TrailDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trail_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
